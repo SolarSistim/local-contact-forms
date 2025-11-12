21d9a34d8490c87f80d355992fe3862ee5c13401
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2025-11-12 14:46:48 -0600

    Adjusted loader to be prettier

--- frontend/src/app/components/loader/loader.component.ts
+++ frontend/src/app/components/loader/loader.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
@@ -8,6 +9,22 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss']
 })
-export class LoaderComponent {
+export class LoaderComponent implements OnInit, OnDestroy {
   @Input() message: string = 'Loading...';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    // Hide body scrolling when loader is visible (only in browser)
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.style.overflow = 'hidden';
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Restore body scrolling when loader is destroyed (only in browser)
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.style.overflow = 'auto';
+    }
+  }
 }
